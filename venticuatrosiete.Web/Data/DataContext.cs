3db07c094bfa2324a0e27da2bc912fe035020ec6
jonathanvelasquez <42391288+jonathanvelasquez@users.noreply.github.com>
2019-08-15 16:26:44 -0500

    Controller products MVC

---
+++ venticuatrosiete.Web/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using venticuatrosiete.Web.Data.Entities;
+
+namespace venticuatrosiete.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+}
