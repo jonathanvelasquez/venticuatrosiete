7da59bd2c3e45ac334173b0bb0b515d6585a98cd
jonathanvelasquez <42391288+jonathanvelasquez@users.noreply.github.com>
2019-08-15 15:44:21 -0500

    Create solution venticuatrosiete

---
+++ venticuatrosiete.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace venticuatrosiete.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ venticuatrosiete.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace venticuatrosiete.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
