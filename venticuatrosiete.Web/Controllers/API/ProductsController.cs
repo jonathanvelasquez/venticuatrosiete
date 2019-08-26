d6ef10c63eb4076969367d2c4a02f9da0668bd3b
jonathanvelasquez <42391288+jonathanvelasquez@users.noreply.github.com>
2019-08-26 16:39:26 -0500

    API Products Controller

---
+++ venticuatrosiete.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿namespace venticuatrosiete.Web.Controllers.API
+{
+    using Microsoft.AspNetCore.Mvc;
+    using venticuatrosiete.Web.Data;
+
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return this.Ok(_productRepository.GetAll());
+        }
+
+    }
+}
